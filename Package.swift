7cdc917288559588515733d01335374b06264c57
BR <snkp002@gmail.com>
2025-01-14 18:04:35 +0800

    style: formatting in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,18 +6,10 @@ import PackageDescription
 let package = Package(
     name: "BRNetwork",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "BRNetwork",
-            targets: ["BRNetwork"]),
+        .library(name: "BRNetwork", targets: ["BRNetwork"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "BRNetwork"),
-        .testTarget(
-            name: "BRNetworkTests",
-            dependencies: ["BRNetwork"]),
+        .target(name: "BRNetwork"),
+        .testTarget(name: "BRNetworkTests", dependencies: ["BRNetwork"]),
     ]
 )
