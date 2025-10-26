5268767b96e07bc3f24b1993369333001942396d
BR <snkp002@gmail.com>
2025-10-26 15:17:37 +0800

    chore: 設定 platforms support in Package (iOS 13)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BRNetwork",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         .library(name: "BRNetwork", targets: ["BRNetwork"]),
     ],
